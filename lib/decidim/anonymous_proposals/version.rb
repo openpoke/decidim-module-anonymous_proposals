8e2604063670377ec1ee5d3b8e63b49fff3d421f
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2025-10-27 12:20:10 +0100

    Upgrade 0.30 (#2)
    * upgrade version
    
    * fix specs

--- lib/decidim/anonymous_proposals/version.rb
+++ lib/decidim/anonymous_proposals/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module AnonymousProposals
     def self.version
-      "0.29"
+      "0.30"
     end
 
     def self.decidim_version
-      ">= 0.29"
+      ">= 0.30"
     end
   end
 end
