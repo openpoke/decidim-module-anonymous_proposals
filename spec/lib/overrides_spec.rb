f2b5abe5ad5747c9ae59035b8920a5fda25ca472
elviabth <ejbenedith@gmail.com>
2025-10-27 11:37:16 +0100

    fix specs

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -10,9 +10,9 @@ checksums = [
     package: "decidim-proposals",
     files: {
       "/app/views/decidim/proposals/proposals/new.html.erb" => "ba8c88bdf07061fda1962c54868c9131",
-      "/app/views/decidim/proposals/proposals/index.html.erb" => "e7a4c4d9f000d17c68ae9eeddde69094",
+      "/app/views/decidim/proposals/proposals/index.html.erb" => "573b29b837fdd0f283be5be942c05aac",
       "/app/views/decidim/proposals/proposals/edit_draft.html.erb" => "3e51c7a14d250a1abef0753a1db7661b",
-      "/app/views/decidim/proposals/proposals/_edit_form_fields.html.erb" => "5907a92b0adf9a0cd5f1e5241317cd48"
+      "/app/views/decidim/proposals/proposals/_edit_form_fields.html.erb" => "127a1a65daad85d7b6f05e689c34ca43"
     }
   }
 ]
