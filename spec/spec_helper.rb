e88c68745ac2fb00c0657f042d4e5f76788ba3f9
elviabth <ejbenedith@gmail.com>
2026-01-09 14:53:00 +0100

    make decidiim-initiatives require optional in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require "decidim/dev"
 begin
   require "decidim/initiatives"
 rescue LoadError
-  warn "Optional dependency 'decidim-initiatives' not found; some specs may be skipped"
 end
 
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
