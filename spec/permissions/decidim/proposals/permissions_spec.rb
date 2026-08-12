92c69c1d9777b778862eaa728967bf3673f87e37
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-08-12 19:53:28 +0300

    Add addtional specs to test admin settings

---
+++ spec/permissions/decidim/proposals/permissions_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Proposals::Permissions do
+  subject { described_class.new(user, permission_action, context).permissions.allowed? }
+
+  let(:permission_action) { Decidim::PermissionAction.new(**action) }
+  let(:user) { create(:user, organization: proposal_component.organization) }
+  let(:context) do
+    {
+      current_component: proposal_component,
+      current_settings:,
+      component_settings:,
+      coauthor:
+    }
+  end
+  let(:coauthor) { nil }
+  let(:proposal_component) { create(:proposal_component) }
+  let(:component_settings) do
+    double(
+      vote_limit: 2,
+      anonymous_proposals_enabled?: anonymous_proposals_enabled,
+      anonymous_proposal_start_time:,
+      anonymous_proposal_end_time:
+    )
+  end
+  let(:current_settings) do
+    double(settings.merge(extra_settings))
+  end
+  let(:settings) do
+    {
+      creation_enabled?: creation_enabled
+    }
+  end
+  let(:anonymous_proposal_end_time) { nil }
+  let(:anonymous_proposal_start_time) { nil }
+  let(:anonymous_proposals_enabled) { false }
+  let(:extra_settings) { {} }
+  let(:action) { { scope: :public, action: :create, subject: :proposal } }
+  let(:creation_enabled) { true }
+
+  context "when creating a proposal" do
+    context "and creation is disabled" do
+      let(:creation_enabled) { false }
+
+      it { is_expected.to be false }
+    end
+
+    context "and user is authorized" do
+      it { is_expected.to be true }
+    end
+  end
+
+  context "when accepting annonymous proposals" do
+    let(:user) { create(:user, :ephemeral, organization: proposal_component.organization) }
+    let(:anonymous_proposals_enabled) { true }
+
+    context "and creation is disabled" do
+      let(:creation_enabled) { false }
+
+      it { is_expected.to be false }
+    end
+
+    context "and user sis set" do
+      it { is_expected.to be true }
+    end
+
+    context "and time start is set, but end is not" do
+      let(:anonymous_proposal_start_time) { "10" }
+
+      it { is_expected.to be true }
+    end
+
+    context "and time end is set, but start is not" do
+      let(:anonymous_proposal_end_time) { "00" }
+
+      it { is_expected.to be true }
+    end
+
+    context "and start time and end time is set" do
+      let(:anonymous_proposal_start_time) { "10" }
+      let(:anonymous_proposal_end_time) { "12" }
+
+      context "and within timeframe" do
+        it { travel_to(Time.zone.now.beginning_of_day + 11.hours) { is_expected.to be true } }
+      end
+
+      context "and outside timeframe" do
+        it "submits before" do
+          travel_to(Time.zone.now.beginning_of_day + 9.hours + 59.minutes + 59.seconds) { expect(subject).to be false }
+        end
+
+        it "submits after" do
+          travel_to(Time.zone.now.beginning_of_day + 12.hours + 1.second) { expect(subject).to be false }
+        end
+      end
+    end
+  end
+end
---
+++ spec/system/admin/admin_manages_component_spec.rb
@@ -0,0 +1,144 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin manages component publication" do # rubocop:disable RSpec/DescribeClass
+  include_context "when managing a component as an admin" do
+    let!(:component) { create(:proposal_component, participatory_space:) }
+    let(:title) { translated(current_component.name) }
+
+    context "when creating a component", :versioning do
+      it "can create proposal" do
+        expect(participatory_space.organization.available_authorizations).not_to include("anonymous_proposals_handler")
+
+        expect(component.reload.organization.available_authorizations).not_to include("anonymous_proposals_handler")
+        visit decidim_admin_participatory_processes.components_path(component.participatory_space)
+        within ".sidebar-menu" do
+          click_on "Components"
+        end
+
+        click_on "Add component"
+        within "#add-component-dropdown" do
+          click_on "Proposals"
+        end
+
+        step_settings = find_by_id("step_settings")
+        page.scroll_to step_settings
+
+        expect(page).to have_content("Allow anonymous users to create proposals")
+        check "Allow anonymous users to create proposals"
+
+        click_on "Add component"
+
+        expect(page).to have_admin_callout("Component created successfully. You can add a content block for this component in the home of the space.")
+
+        # for some reason, we cannot test the component settings
+        expect(participatory_space.reload.organization.available_authorizations).to include("anonymous_proposals_handler")
+      end
+    end
+
+    context "when updating a component" do
+      it "can set the proposal to accept anonymous proposals" do
+        expect(component.reload.organization.available_authorizations).not_to include("anonymous_proposals_handler")
+        expect(component.permissions).to be_nil
+
+        visit decidim_admin_participatory_processes.components_path(component.participatory_space)
+        within ".sidebar-menu" do
+          click_on "Components"
+        end
+
+        within "tr", text: title do
+          find("button[data-controller='dropdown']").click
+          click_on "Configure"
+        end
+
+        step_settings = find_by_id("step_settings")
+        page.scroll_to step_settings
+
+        expect(page).to have_content("Allow anonymous users to create proposals")
+        check "Allow anonymous users to create proposals"
+
+        click_on "Update"
+
+        expect(page).to have_admin_callout("The component was updated successfully.")
+
+        expect(component.reload.settings).to be_anonymous_proposals_enabled
+        expect(component.reload.organization.available_authorizations).to include("anonymous_proposals_handler")
+        expect(component.permissions.dig("withdraw", "authorization_handlers")).to include({ "anonymous_proposals_handler" => {} })
+
+        within "tr", text: title do
+          find("button[data-controller='dropdown']").click
+          click_on "Configure"
+        end
+
+        step_settings = find_by_id("step_settings")
+        page.scroll_to step_settings
+
+        expect(page).to have_content("Allow anonymous users to create proposals")
+        uncheck "Allow anonymous users to create proposals"
+
+        click_on "Update"
+
+        expect(page).to have_admin_callout("The component was updated successfully.")
+
+        expect(component.reload.settings).not_to be_anonymous_proposals_enabled
+        expect(component.reload.organization.available_authorizations).to include("anonymous_proposals_handler")
+        expect(component.reload.permissions).to be_empty
+      end
+
+      it "can set the proposal to accept anonymous proposals within a timeframe" do
+        expect(component.reload.organization.available_authorizations).not_to include("anonymous_proposals_handler")
+        expect(component.permissions).to be_nil
+
+        visit decidim_admin_participatory_processes.components_path(component.participatory_space)
+        within ".sidebar-menu" do
+          click_on "Components"
+        end
+
+        within "tr", text: title do
+          find("button[data-controller='dropdown']").click
+          click_on "Configure"
+        end
+
+        step_settings = find_by_id("step_settings")
+        page.scroll_to step_settings
+
+        expect(page).to have_content("Allow anonymous users to create proposals")
+        check "Allow anonymous users to create proposals"
+        select "01:00", from: :component_settings_anonymous_proposal_start_time
+        select "11:00", from: :component_settings_anonymous_proposal_end_time
+
+        click_on "Update"
+
+        expect(component.reload.settings).to be_anonymous_proposals_enabled
+        expect(component.reload.settings.anonymous_proposal_start_time).to eq "1"
+        expect(component.reload.settings.anonymous_proposal_end_time).to eq "11"
+        expect(component.reload.organization.available_authorizations).to include("anonymous_proposals_handler")
+        expect(component.permissions.dig("withdraw", "authorization_handlers")).to include({ "anonymous_proposals_handler" => {} })
+
+        within "tr", text: title do
+          find("button[data-controller='dropdown']").click
+          click_on "Configure"
+        end
+
+        step_settings = find_by_id("step_settings")
+        page.scroll_to step_settings
+
+        expect(page).to have_content("Allow anonymous users to create proposals")
+        uncheck "Allow anonymous users to create proposals"
+        select "", from: :component_settings_anonymous_proposal_start_time
+        select "", from: :component_settings_anonymous_proposal_end_time
+
+        click_on "Update"
+
+        expect(page).to have_admin_callout("The component was updated successfully.")
+
+        expect(component.reload.settings).not_to be_anonymous_proposals_enabled
+        expect(component.reload.settings.anonymous_proposal_start_time).to eq("")
+        expect(component.reload.settings.anonymous_proposal_end_time).to eq("")
+        expect(component.reload.organization.available_authorizations).to include("anonymous_proposals_handler")
+        expect(component.reload.permissions).to be_empty
+      end
+    end
+  end
+end
